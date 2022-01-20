b58ee98ce4f2be7848739868dbfbe2b2ba8d3363
amrmoussa17 <amrmamdoh@hotmail.com>
2022-01-20 21:06:33 +0200

    added settings box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,23 @@
+// make settings box appear on settings icon click
+const settingsIcon = document.querySelector(".settings-container .icon");
+const iconBox = document.querySelector(".settings-container .icon-box");
+const settingsBox = document.querySelector(".settings-container"); 
+iconBox.addEventListener('click', () => {
+    settingsBox.classList.toggle('open');
+    settingsIcon.classList.toggle('fa-spin');
+})
+// color box settings
+colorBox = document.querySelector(".color-settings .colors-list");
+colorItems = document.querySelectorAll(".color-settings .colors-list li")
+colorBox.addEventListener('click', (evt) => {
+    const colorOption = evt.target.dataset.color;
+    document.documentElement.style.setProperty('--main-color', colorOption);
+    colorItems.forEach( (item) => {
+        item.classList.remove('active');
+    })
+    evt.target.classList.add('active');
+});
+
 // setting random background images for landing section
 const landingSection = document.querySelector('.landing');
 const imgsArray = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
